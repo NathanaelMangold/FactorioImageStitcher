1be1e4e7c7cbcc54ab682ef4b0728473b99da299
Nathanael <Nathanael.Mangold98@gmail.com>
2022-01-17 10:22:40 +0100

    Updated git ignore + cleanup Code

--- imagestitcher.py
+++ imagestitcher.py
@@ -1,84 +1,68 @@
 from multiprocessing import Process
-from tkinter.constants import HORIZONTAL
 from PIL import Image
 import os
 import re
 
 def main():
+    # Print Welcome Text
     print("=====================================================================")                                                                 
     print(" _____         _           _        _____ _   _ _       _            ")
     print("|   __|___ ___| |_ ___ ___|_|___   |   __| |_|_| |_ ___| |_ ___ ___  ")
     print("|   __| .'|  _|  _| . |  _| | . |  |__   |  _| |  _|  _|   | -_|  _| ")
     print("|__|  |__,|___|_| |___|_| |_|___|  |_____|_| |_|_| |___|_|_|___|_|   ")
     print("=====================================================================")
-    print("Starting Factorio Image Stitcher!")
+    print("Starting Factorio Image Stitcher!                        by Nathanael")
 
     # Get Configuration Data
-    inputPathRaw = input("Input Folder:")
-    if not inputPathRaw:
-        inputPathRaw = "D:\DEV\FactorioImageStitcher\input"
-    
-    if not os.path.exists(inputPathRaw):
-        print(f"Invalid Input Path! '{inputPathRaw}'")
-        return
+    input_path = getDirectoryPath("Input Folder: ")
+    output_path = getDirectoryPath("Output Folder: ")
 
-    outputPathRaw = input("Output Folder:")
-    if not outputPathRaw:
-        outputPathRaw = "D:\DEV\FactorioImageStitcher\output"
-
-    if not os.path.exists(outputPathRaw):
-        print(f"Invalid Output Path! '{outputPathRaw}'")
-        return
-
-    inputPath  = inputPathRaw
-    outputPath = outputPathRaw
-
-    # Process Count
-    inputProcessCountRaw = input("Process Count:")
-    if not inputProcessCountRaw.isdigit():
-        inputProcessCountRaw = 8
-    processCount = int(inputProcessCountRaw)
-
-    # Test on https://regex101.com/ - Pattern match: screenshotNUMBER_xNUMBER_yNUMBER.png e.g. screenshot1234_x0_y13.png
+    # To understand regex better use e.g. https://regex101.com/
+    # Pattern: screenshotNUMBER_xNUMBER_yNUMBER.png e.g. screenshot1234_x0_y13.png
+    # Group 0 -> Screenshot ID
+    # Group 1 -> X Coordinate
+    # Group 2 -> Y Coordinate
     pattern = re.compile(r"screenshot(\d*)_x(\d*)_y(\d*).png")
 
+    # Multi Processing
     processes = []
+    process_count = 8
 
-    currentScreenshotID :str = ""
+    current_screenshot_id :str = ""
 
-    totalFileCount = 0
-    processedFiles = 0
+    total_file_count = 0
+    processed_files = 0
 
     # Get Total File Count of folder
-    with os.scandir(inputPath) as it:
-        totalFileCount = sum(1 for _ in it)
+    with os.scandir(input_path) as it:
+        total_file_count = sum(1 for _ in it)
 
-    if totalFileCount == 0:
+    if total_file_count == 0:
         print("No files in selected input folder!")
         return
 
-    with os.scandir(inputPath) as it:
+    with os.scandir(input_path) as it:
         # Image File Name, (x,y)
-        imageData = []
+        image_data = []
         for entry in it:
             # Check for validity 
-            fileName :str = entry.name
-            match = pattern.match(fileName)
+            file_name :str = entry.name
+            match = pattern.match(file_name)
             if match is None:
-                print(f"File: '{fileName}' is not a valid screenshot!")
+                print(f"File: '{file_name}' is not a valid screenshot!")
                 continue
 
-            #print(f"ID: {match.group(1)} | x: {match.group(2)} | y: {match.group(3)} ")
-            screenshotID = match.group(1)
+            screenshot_id = match.group(1)
             coords = (match.group(2), match.group(3))
 
             # Only relevant for first iteration
-            if(not currentScreenshotID):
-                currentScreenshotID = screenshotID
+            if(not current_screenshot_id):
+                current_screenshot_id = screenshot_id
 
             # Stitch image
-            if(screenshotID != currentScreenshotID):
-                while len(processes) > processCount:
+            if(screenshot_id != current_screenshot_id):
+                # Makes sure only a fixed amount of processes is started
+                while len(processes) > process_count:
                     processToRemove = []
                     for pro in processes:
                         pro.join(timeout=0)
@@ -87,72 +71,70 @@ def main():
                     for pro in processToRemove:
                         processes.remove(pro)
 
-
-                #print("Activ running processes:", len(processes))
-
-                #if len(processes) > processCount:
-                #    for process in processes:
-                #        process.join()
-                #    processes.clear()
-
-                process = Process(target=stitchImage, args=(imageData, outputPath, currentScreenshotID))
+                # Start process with image
+                process = Process(target=stitchImage, args=(image_data, output_path, current_screenshot_id))
                 process.start()
                 processes.append(process)
-                #stitchImage(imageData, outputPath, currentScreenshotID)
 
-                imageData.clear()
-                currentScreenshotID = screenshotID
+                image_data.clear()
+                current_screenshot_id = screenshot_id
 
                 #print(f"Progress: {processedFiles}/{totalFileCount}")
                 
 
-            imageData.append([inputPath + r"\\" + fileName, coords])
-            processedFiles += 1
+            image_data.append([input_path + r"\\" + file_name, coords])
+            processed_files += 1
 
         for process in processes:
             process.join()
 
         # Stitch last image once loop finished
-        stitchImage(imageData, outputPath, currentScreenshotID)
+        stitchImage(image_data, output_path, current_screenshot_id)
 
 
 
-def stitchImage(imageParts, outputPath, currentScreenshotID):
+def stitchImage(image_parts, output_path, current_screenshot_id):
     # Get Information about image package
-    image :Image = Image.open(imageParts[0][0])
+    image :Image = Image.open(image_parts[0][0])
     (width, height) = image.size
     image.close()
 
     maxX :int = 0
     maxY :int = 0
+
     # find out max x and max y
-    for entry in imageParts:
+    for entry in image_parts:
         maxX = max(maxX, int(entry[1][0]))
         maxY = max(maxY, int(entry[1][1]))
-
-    #print(f"Max X Coord: {maxX} ")
-    #print(f"Max Y Coord: {maxY} ")
 
     imageWidth = width * int(maxY) + width
     imageHeight = height * int(maxX) + height
     sizeTouple = (imageWidth, imageHeight)
 
-    #print(f"Target Image Size: {sizeTouple}")
-
     # Stitch
     stitched = Image.new('RGB', sizeTouple)
-    for entry in imageParts:
+    for entry in image_parts:
         image :Image = Image.open(entry[0])
-        # print(f"Putting Image {entry[1][0]}, {entry[1][1]}")
+
         x = int(entry[1][0]) * width
         y = int(entry[1][1]) * height
 
-        #print(f"Putting Picture {x}, {y}")
         stitched.paste(im= image, box=(x, y))
         image.close()
-            
 
-    stitched.save(outputPath + "\\" + currentScreenshotID + ".png", "PNG")
+    stitched.save(output_path + "\\" + current_screenshot_id + ".png", "PNG")
+
+def getDirectoryPath(prompt_message :str):
+    while True:
+        directory_path = input(prompt_message)
+        if not os.path.isdir(directory_path):
+            if os.path.isfile(directory_path):
+                print("Selected path is a file. Please select a directory instead.")
+
+            print(f"Invalid directory path! '{directory_path}'")
+        else:
+            print(f"Valid path selected: '{directory_path}'")
+            return directory_path
 
 if __name__ == "__main__":
     main()
